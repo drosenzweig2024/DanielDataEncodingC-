7219c96607be39ad02d9c54ddf202d6d8ec02880
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-01-14 08:52:37 -0800

    work

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace DanielDataEncodingC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
