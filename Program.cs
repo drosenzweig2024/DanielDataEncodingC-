4340811ca2b9ad9a1fecf5ef33d44cc2b7e20946
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-01-17 20:50:48 -0800

    Part 1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,28 @@ namespace DanielDataEncodingC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           char ch;
+           ch = 'A';
+           Console.WriteLine($"ch is the storing value: {ch}");
+
+           int ch_decimal;
+           ch_decimal = ch;
+           Console.WriteLine($"The decimal Value of ch is: {ch_decimal}");
+
+           int ToConvert;
+           Console.WriteLine("Enter An Integer: ");
+           ToConvert = int.Parse(Console.ReadLine());
+
+           char asChar;
+           asChar = (char)ToConvert;
+           Console.WriteLine($"The integer {ToConvert} cast to a char {asChar}");
+
+           char b;
+           b = (char) ('A' + 1);
+           Console.WriteLine($"'a' + 1 = {b}");
+            }
+
+
         }
     }
-}
+
